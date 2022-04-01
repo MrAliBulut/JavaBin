34f16e70e6d553e98f190f1d6b4c338bdd3206c6
Ali Bulut <95712617+MrAliBulut@users.noreply.github.com>
2022-04-01 13:27:02 +0300

    Update vektor.java
    Okunabilirliğini arttırdım.

--- vektor.java
+++ vektor.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
+
 public class vektor {
+    
     public static void main(String[] args){
+        
         Scanner input= new Scanner(System.in);
         System.out.println("Merhaba. Bugün sizlerle vektörleri dizilerde saklayıp daha sonra onlar ile işlem yapacağız.");
+        
+            //Vektörleri saklayacağımız dizi. 2 vektörün 3 eksendeki verilerini burada tutacağız.
         double v[][]= new double[2][3];
         
         
+            //Vektörlerin değerlerini girdi olarak alıp tanımladığımız v dizisine saklıyoruz.
         for(int i=0; i<2; i++){
             
             
@@ -20,8 +26,9 @@ public class vektor {
             }
         }
 
-
+            // Vektörel çarpım 41. satıra kadar
         double x1=0,x2=0,y1=0,y2=0,z1=0,z2=0;
+        
         x1= v[0][1] * v[1][2];
         x2= v[0][2] * v[1][1];
 
@@ -31,16 +38,19 @@ public class vektor {
         z1= v[0][0] * v[1][1];
         z2= v[0][1] * v[1][0];
 
-        double c1= x1-x2;
-        double c2= (y1-y2)*-1;
-        double c3= z1-z2;
+        double c1= x1-x2;           //Sonuç vektörünün x eksenindeki değeri
+        double c2= (y1-y2)*-1;      //Sonuç vektörünün y eksenindeki değeri
+        double c3= z1-z2;           //Sonuç vektörünün z eksenindeki değeri
+            
 
+            
+            //Eksen değerlerini bulduğumuz sonuç vektörümüzü ayrı bir dizide saklıyoruz.
         double sonuc[]= {c1,c2,c3};
 
+            //Sonuç vektörümüzü ekrana yazdırıyoruz.
         System.out.print("Girdiğiniz vektör değerlerinin vektörel çarpımı= ");
         for(int s=0; s<3; s++){
             System.out.print(sonuc[s]+",");
         }
     }   
-    
 }
